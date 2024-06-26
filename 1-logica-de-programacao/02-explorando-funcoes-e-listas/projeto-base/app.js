b35b43b180f5abc93c314db66b9561bb50584453
Marx Santos <marx.devweb@gmail.com>
2024-06-26 19:17:34 -0300

    Update app.js

---
+++ 1-logica-de-programacao/02-explorando-funcoes-e-listas/projeto-base/app.js
@@ -0,0 +1,10 @@
+let titulo = document.querySelector('h1'); //Selecionando a tag h1
+titulo.innerHTML = "JOGO DO NÚMERO SECRETO"; //inserindo o texto 
+
+let paragrafo = document.querySelector('p'); //Selecionando a tag p
+paragrafo.innerHTML = "Escolha um número entre 1 e 10:"; //inserindo o texto
+
+//CRIANDO UMA FUNÇÃO PARA VERIFICAR SE O NÚMERO INFORMADO PELO É IGUAL AO NÚMERO SECRETO.
+function verificarChute() {
+    console.log("O botão foi clicado")
+}
